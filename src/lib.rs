bae0d051da1effb1c33b52dd41dfe72282e1bf9b
Michael Hinton <hinton@schedmd.com>
2020-02-08 18:44:17 -0700

    Create log_file macro to reduce repeated code
    Pass in rc in case it fails, but to allow it to still be flexible.
    Make log_file variadic, so it's a true wrapper of write!().
    Factor out the prefix into a const.
    Remove ineffective println!s.
    Run rustfmt.

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ const SLURM_SUCCESS: c_int = 0;
 const SLURM_FAILURE: c_int = -1;
 
 const JOB_SUBMIT_FILE: &str = "job_submit_rust.log";
+const LOG_PREFIX: &str = "Job Submit Rust: ";
 
 // Export strings for plugin interface
 // https://users.rust-lang.org/t/how-to-export-string-symbol-not-function-to-c/26039
@@ -27,6 +28,48 @@ pub static plugin_type: [u8; 16] = *b"job_submit/rust\0";
 #[used]
 pub static plugin_version: u32 = 0x140200; /* i.e. Slurm 20.02.0 */
 
+/*
+ * Create a print-to-file wrapper that also adds a custom prefix and a time
+ * stamp. Importantly, on failure, the value for rc found in the local scope is
+ * set to SLURM_FAILURE.
+ *
+ * See:
+ * https://doc.rust-lang.org/reference/macros-by-example.html
+ * https://github.com/rust-lang/rust/blob/07a34df18b437319a7ff510077bbab95cf7ec6bc/src/libstd/macros.rs#L62-L64
+ * https://doc.rust-lang.org/std/macro.format_args.html
+ * https://doc.rust-lang.org/rust-by-example/macros/variadics.html
+ * https://doc.rust-lang.org/book/ch19-06-macros.html
+ * https://stackoverflow.com/questions/34373169/how-do-i-create-a-rust-macro-with-optional-parameters-using-repetitions
+ */
+macro_rules! log_file {
+    ($rc:expr, $($arg:tt)*) => ({
+        let file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(JOB_SUBMIT_FILE);
+        match file {
+            Ok(file) => {
+                let mut file = file;
+                let now: DateTime<Local> = Local::now();
+                let result = write!(
+                    file,
+                    "{}: {}{}\n",
+                    now, LOG_PREFIX, format_args!($($arg)*)
+                );
+                match result {
+                    Ok(_) => {}
+                    Err(_) => {
+                        $rc = SLURM_FAILURE;
+                    },
+                };
+            },
+            Err(_) => {
+                $rc = SLURM_FAILURE;
+            },
+        };
+    });
+}
+
 // C prototype:
 // extern int job_submit(job_desc_msg_t *job_desc, u32 submit_uid,
 //               u8 **err_msg)
@@ -36,32 +79,13 @@ pub extern "C" fn job_submit(
     submit_uid: u32,
     _err_msg: *mut c_void,
 ) -> c_int {
-    println!(
-        "Rust has infected Slurm! Submitted by user {}\n",
+    let mut rc = SLURM_SUCCESS;
+    log_file!(
+        rc,
+        "Slurm has called job_submit() in Rust! uid={}",
         submit_uid
     );
-
-    let file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(JOB_SUBMIT_FILE);
-    let mut file = match file {
-        Ok(file) => file,
-        Err(_) => return SLURM_FAILURE,
-    };
-
-    let now: DateTime<Local> = Local::now();
-    let result = write!(
-        file,
-        "{}: Job Submit Rust: Slurm has called job_submit() in Rust! uid={}\n",
-        now, submit_uid
-    );
-    match result {
-        Ok(_) => {}
-        Err(_) => return SLURM_FAILURE,
-    };
-
-    SLURM_SUCCESS
+    rc
 }
 
 // C prototype:
@@ -73,70 +97,27 @@ pub extern "C" fn job_modify(
     _job_ptr: *mut c_void,
     submit_uid: u32,
 ) -> c_int {
-    println!("Rust has infected Slurm! Modified by user {}\n", submit_uid);
-
-    let file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(JOB_SUBMIT_FILE);
-    let mut file = match file {
-        Ok(file) => file,
-        Err(_) => return SLURM_FAILURE,
-    };
-
-    let now: DateTime<Local> = Local::now();
-    let result = write!(
-        file,
-        "{}: Job Submit Rust: Slurm has called job_modify() in Rust! uid={}\n",
-        now, submit_uid
+    let mut rc = SLURM_SUCCESS;
+    log_file!(
+        rc,
+        "Slurm has called job_submit() in Rust! uid={}",
+        submit_uid
     );
-    match result {
-        Ok(_) => {}
-        Err(_) => return SLURM_FAILURE,
-    };
-    SLURM_SUCCESS
+    rc
 }
 
 #[no_mangle]
 pub extern "C" fn fini() {
-    let file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(JOB_SUBMIT_FILE);
-    let mut file = match file {
-        Ok(file) => file,
-        Err(_) => return,
-    };
-
-    let now: DateTime<Local> = Local::now();
-    let result = write!(file, "{}: Job Submit Rust: fini\n", now);
-    match result {
-        Ok(_) => {}
-        Err(_) => return,
-    };
+    let mut _rc = SLURM_SUCCESS;
+    log_file!(_rc, "fini");
 }
 
 #[no_mangle]
 pub extern "C" fn init() -> c_int {
-    let file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(JOB_SUBMIT_FILE);
-    let mut file = match file {
-        Ok(file) => file,
-        Err(_) => return SLURM_FAILURE,
-    };
-
-    let now: DateTime<Local> = Local::now();
-    let result = write!(file, "{}: Job Submit Rust: init\n", now);
-    match result {
-        Ok(_) => {},
-        Err(_) => return SLURM_FAILURE,
-    };
-
-    SLURM_SUCCESS
+    let mut rc = SLURM_SUCCESS;
+    log_file!(rc, "init");
+    rc
 }
-
 
 // #[cfg(test)]
 // mod tests {
